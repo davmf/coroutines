c893cae712cd10037fdce5e8661d4fb33f58d88d
David Fyfe <david@davidfyfe.net>
2024-02-04 08:15:01 +1100

    Add structural diagram

--- main.cpp
+++ main.cpp
@@ -10,12 +10,33 @@ public:
 struct coroutine_result::promise_type {
 
     auto get_return_object() {
-        std::cout << promise_type::get_return_object() << std::endl;
+        std::cout << "promise_type::get_return_object()" << std::endl;
         return coroutine_result{42};
+    }
+
+    auto initial_suspend() {
+        std::cout << "promise_type::initial_suspend()" << std::endl;
+        return std::suspend_never{};
+    }
+
+    auto final_suspend() {
+        std::cout << "promise_type::final_suspend()" << std::endl;
+
+    }
+
+    auto return_void() {
+        std::cout << "promise_type::return_void()" << std::endl;
+
+    }
+
+    auto unhandled_exception() {
+        std::cout << "promise_type::unhandled_exception()" << std::endl;
+        std::terminate();
     }
 }
 
 
 int main(int, char**){
     std::cout << "Hello, from coro1!\n";
+    return 0;
 }
