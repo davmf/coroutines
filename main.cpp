a3d47698cd6fe946b93348c10a13f5c20b81c0d5
David Fyfe <david@davidfyfe.net>
2024-02-05 06:10:42 +1100

    Further simplification

--- main.cpp
+++ main.cpp
@@ -2,48 +2,46 @@
 #include <coroutine>
 
 class coroutine_result {
+
 public:
-    struct promise_type;
-};
+    struct promise_type {
 
-struct coroutine_result::promise_type {
+        auto get_return_object() {
+            std::cout << "promise_type::get_return_object()" << std::endl;
+            return coroutine_result{};
+        }
 
-    auto get_return_object() {
-        std::cout << "promise_type::get_return_object()" << std::endl;
-        return coroutine_result{};
-    }
+        auto initial_suspend() {
+            std::cout << "promise_type::initial_suspend()" << std::endl;
+            return std::suspend_never{};
+        }
 
-    auto initial_suspend() {
-        std::cout << "promise_type::initial_suspend()" << std::endl;
-        return std::suspend_never{};
-    }
+        auto final_suspend() noexcept {
+            std::cout << "promise_type::final_suspend()" << std::endl;
+            return std::suspend_never{};
+        }
 
-    auto final_suspend() noexcept {
-        std::cout << "promise_type::final_suspend()" << std::endl;
-        return std::suspend_never{};
-    }
+        auto return_void() {
+            std::cout << "promise_type::return_void()" << std::endl;
+        }
 
-    auto return_void() {
-        std::cout << "promise_type::return_void()" << std::endl;
-    }
-
-    auto unhandled_exception() {
-        std::cout << "promise_type::unhandled_exception()" << std::endl;
-        std::terminate();
-    }
+        auto unhandled_exception() {
+            std::cout << "promise_type::unhandled_exception()" << std::endl;
+            std::terminate();
+        }
+    };
 };
 
 
 coroutine_result my_coro() {
-    std::cout << "my_coro::initial_suspend()" << std::endl;
-    co_await std::suspend_never{};
-    std::cout << "my_coro::final_suspend()" << std::endl;
+    std::cout << "During my_coro" << std::endl;
+    co_return;
 }
 
 
 int main(int, char**){
-    std::cout << "Hello, from before coro1!\n";
+    std::cout << "Before my_coro\n";
     auto result = my_coro();
-    std::cout << "Hello, from after coro1!\n";
+    std::cout << "After my_coro\n";
     return 0;
 }
