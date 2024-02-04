259b480b83cb4c3febb95adbb7b50a891bb9dd31
David Fyfe <david@davidfyfe.net>
2024-02-05 06:03:03 +1100

    Working

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <memory>
 #include <coroutine>
 
 class coroutine_result {
@@ -21,12 +20,11 @@ struct coroutine_result::promise_type {
 
     auto final_suspend() noexcept {
         std::cout << "promise_type::final_suspend()" << std::endl;
-
+        return std::suspend_never{};
     }
 
     auto return_void() {
         std::cout << "promise_type::return_void()" << std::endl;
-
     }
 
     auto unhandled_exception() {
@@ -38,12 +36,14 @@ struct coroutine_result::promise_type {
 
 coroutine_result my_coro() {
     std::cout << "my_coro::initial_suspend()" << std::endl;
-    co_await std::suspend_always{};
+    co_await std::suspend_never{};
     std::cout << "my_coro::final_suspend()" << std::endl;
 }
 
 
 int main(int, char**){
-    std::cout << "Hello, from coro1!\n";
+    std::cout << "Hello, from before coro1!\n";
+    auto result = my_coro();
+    std::cout << "Hello, from after coro1!\n";
     return 0;
 }
